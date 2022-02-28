ed08bcf3aaad9080439b201e452bd97d1a93f575
mepweb <97809456+mepweb@users.noreply.github.com>
2022-02-28 18:40:02 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const pubgm_btn = document.getElementById('pubgm')
-const purchase_btn = document.getElementById('purchase')
-const shop_btn = document.getElementById('shop')
-
-pubgm_btn.onclick =()=>{
-    location.href = "https://www.midasbuy.com/midasbuy/tr/homepage/pubgm";
-}
