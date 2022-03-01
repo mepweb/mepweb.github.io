62b6c1e9018386fc0b1d4e940d668cc5d723b6b6
mepweb <97809456+mepweb@users.noreply.github.com>
2022-03-01 20:38:48 +0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -11,3 +11,63 @@ purchase_btn.onclick =()=>{
 shop_btn.onclick =()=>{
     location.href = "https://www.midasbuy.com/midasbuy/tr/shop/pubgm";
 }
+function get(str) {
+    $("#totalbp").html(str)
+}
+
+$(".ucler").click(function(){
+    const ids = "#"+this.id
+    $(ids).addClass('ucler_new')
+    if (ids == "#600") {
+        $('#3000').removeClass('ucler_new')
+        $('#6000').removeClass('ucler_new')
+        $('#15000').removeClass('ucler_new')
+    }
+    if (ids == "#3000") {
+        $('#600').removeClass('ucler_new')
+        $('#6000').removeClass('ucler_new')
+        $('#15000').removeClass('ucler_new')
+    }
+    if (ids == "#6000") {
+        $('#600').removeClass('ucler_new')
+        $('#3000').removeClass('ucler_new')
+        $('#15000').removeClass('ucler_new')
+    }
+    if (ids == "#15000") {
+        $('#600').removeClass('ucler_new')
+        $('#3000').removeClass('ucler_new')
+        $('#6000').removeClass('ucler_new')
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const activeimg1 = document.getElementById("activeimg1")
+const activeimg2 = document.getElementById("activeimg2")
+const activeimg3 = document.getElementById("activeimg3")
+const activeimg4 = document.getElementById("activeimg4")*/
